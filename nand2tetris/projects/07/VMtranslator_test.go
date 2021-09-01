faa2304b24fb5b356080cbae7c8125524764bb9e
Jordan Chen <chenjor@gmail.com>
2021-09-01 18:54:11 -0400

    Make VMTranslator Tests Build Binary Before Running Tests

--- nand2tetris/projects/07/VMtranslator_test.go
+++ nand2tetris/projects/07/VMtranslator_test.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 	"testing"
 )
+
+func TestMain(m *testing.M) {
+	// Build the VMtranslator binary before running the test suite
+	build := exec.Command("go", "build", ".")
+	err := build.Run()
+	if err != nil {
+		fmt.Printf("could not build VMtranslator: %v", err)
+		os.Exit(1)
+	}
+	os.Exit(m.Run())
+}
 
 func TestSimpleAdd(t *testing.T) {
 	inputPath := "StackArithmetic/SimpleAdd/SimpleAdd.vm"
